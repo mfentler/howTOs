87b6c0f905093236d494b522e9d9cc594b95f8bc
Mario Fentler <mfentler@student.tgm.ac.at>
2018-10-24 09:48:34 +0200

    fixed url error

--- insy5/nodejs/Uebung1/dns/dns-lookup.js
+++ insy5/nodejs/Uebung1/dns/dns-lookup.js
@@ -2,11 +2,10 @@ var dns = require('dns');
 
 if(process.argv.length > 2){
     for(var i = 2; i < process.argv.length; i++){
-        var url = process.argv[i];
-        console.log(url+" : ")
+        let url = process.argv[i];
         dns.lookup(url, function (err, address) {
             if (err) console.log(err)   // Log errors
-            else console.log(JSON.stringify(address))
+            else console.log(url + " : " + JSON.stringify(address))
         });
     }
 }else{
